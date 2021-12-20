f76049323ffd2eae808d13e69ecee58f89cc6975
jhoustonmc <jhouston@student.wethinkcode.co.za>
2021-12-20 09:37:23 +0200

    Push to main

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import path
 config_container_name = "massmartconfigs"
 config_blob_file_name = "config.json"
 
-connect_str = "BlobEndpoint=https://bcxmassmartsurvey.blob.core.windows.net/;QueueEndpoint=https://bcxmassmartsurvey.queue.core.windows.net/;FileEndpoint=https://bcxmassmartsurvey.file.core.windows.net/;TableEndpoint=https://bcxmassmartsurvey.table.core.windows.net/;SharedAccessSignature=sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacupx&se=2021-07-31T20:58:03Z&st=2021-04-12T12:58:03Z&spr=https&sig=qykZavUWd140TzN70ur4IDL4SNWO4WAqU71ZDooqVyk%3D"
+connect_str = "BlobEndpoint=https://bcxmassmartsurvey.blob.core.windows.net/;QueueEndpoint=https://bcxmassmartsurvey.queue.core.windows.net/;FileEndpoint=https://bcxmassmartsurvey.file.core.windows.net/;TableEndpoint=https://bcxmassmartsurvey.table.core.windows.net/;SharedAccessSignature=sv=2020-08-04&ss=bfqt&srt=c&sp=rwdlacupitfx&se=2023-12-13T19:01:44Z&st=2021-12-13T11:01:44Z&spr=https&sig=gPCL21M28s3AQG1r6LPyrbyW957em7w7uOOXSdjmk2Q%3D"
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
 container_client = blob_service_client.get_container_client(container=config_container_name)
